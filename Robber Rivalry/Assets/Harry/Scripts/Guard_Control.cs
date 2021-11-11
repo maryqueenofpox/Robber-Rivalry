5f7e5f5c3f491827a52e9227ef5c6b84ae95755f
Harry <62217558+maryqueenofpox@users.noreply.github.com>
2021-11-11 17:33:30 +0000

    Added more guards, Added new platforms

--- Robber Rivalry/Assets/Harry/Scripts/Guard_Control.cs
+++ Robber Rivalry/Assets/Harry/Scripts/Guard_Control.cs
@@ -36,7 +36,8 @@ public class Guard_Control : MonoBehaviour
 		if (IsPlayer1InVisionDistance() == true && IsPlayer1InVisionAngle() == true)
 		{
             GetComponent<AIDestinationSetter>().enabled = true;
-            aipath.maxSpeed = 10f;
+            GetComponent<AIDestinationSetter>().target = player1;
+            aipath.maxSpeed = 10f;           
         }
         else if (IsPlayer1InVisionDistance() == false && IsPlayer1InVisionAngle() == false)
         {
@@ -44,7 +45,42 @@ public class Guard_Control : MonoBehaviour
             GetComponent<Patrol>().enabled = true;
             aipath.maxSpeed = 3f;
         }
-
+        else if (IsPlayer2InVisionDistance() == true && IsPlayer2InVisionAngle() == true)
+        {
+            GetComponent<AIDestinationSetter>().enabled = true;
+            GetComponent<AIDestinationSetter>().target = player2;
+            aipath.maxSpeed = 10f;
+        }
+        else if (IsPlayer2InVisionDistance() == false && IsPlayer2InVisionAngle() == false)
+        {
+            GetComponent<AIDestinationSetter>().enabled = false;
+            GetComponent<Patrol>().enabled = true;
+            aipath.maxSpeed = 3f;
+        }
+        else if (IsPlayer3InVisionDistance() == true && IsPlayer3InVisionAngle() == true)
+        {
+            GetComponent<AIDestinationSetter>().enabled = true;
+            GetComponent<AIDestinationSetter>().target = player3;
+            aipath.maxSpeed = 10f;
+        }
+        else if (IsPlayer3InVisionDistance() == false && IsPlayer3InVisionAngle() == false)
+        {
+            GetComponent<AIDestinationSetter>().enabled = false;
+            GetComponent<Patrol>().enabled = true;
+            aipath.maxSpeed = 3f;
+        }
+        else if (IsPlayer4InVisionDistance() == true && IsPlayer4InVisionAngle() == true)
+        {
+            GetComponent<AIDestinationSetter>().enabled = true;
+            GetComponent<AIDestinationSetter>().target = player4;
+            aipath.maxSpeed = 10f;
+        }
+        else if (IsPlayer4InVisionDistance() == false && IsPlayer4InVisionAngle() == false)
+        {
+            GetComponent<AIDestinationSetter>().enabled = false;
+            GetComponent<Patrol>().enabled = true;
+            aipath.maxSpeed = 3f;
+        }
     }
 
     protected bool IsPlayer1InVisionDistance()
@@ -56,6 +92,53 @@ public class Guard_Control : MonoBehaviour
     protected bool IsPlayer1InVisionAngle()
     {
         Vector3 playerDirection = player1.transform.position - transform.position;
+        playerDirection = playerDirection.normalized;
+
+        float dot = Vector3.Dot(playerDirection, transform.forward);
+        float angleToPlayer = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        return angleToPlayer <= VisionAngle;
+    }
+
+    protected bool IsPlayer2InVisionDistance()
+    {
+        float distanceToPlayer = (player2.transform.position - transform.position).magnitude;
+        return distanceToPlayer <= VisionDistance;
+    }
+
+    protected bool IsPlayer2InVisionAngle()
+    {
+        Vector3 playerDirection = player2.transform.position - transform.position;
+        playerDirection = playerDirection.normalized;
+
+        float dot = Vector3.Dot(playerDirection, transform.forward);
+        float angleToPlayer = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        return angleToPlayer <= VisionAngle;
+    }
+
+    protected bool IsPlayer3InVisionDistance()
+    {
+        float distanceToPlayer = (player3.transform.position - transform.position).magnitude;
+        return distanceToPlayer <= VisionDistance;
+    }
+
+    protected bool IsPlayer3InVisionAngle()
+    {
+        Vector3 playerDirection = player3.transform.position - transform.position;
+        playerDirection = playerDirection.normalized;
+
+        float dot = Vector3.Dot(playerDirection, transform.forward);
+        float angleToPlayer = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        return angleToPlayer <= VisionAngle;
+    }
+    protected bool IsPlayer4InVisionDistance()
+    {
+        float distanceToPlayer = (player4.transform.position - transform.position).magnitude;
+        return distanceToPlayer <= VisionDistance;
+    }
+
+    protected bool IsPlayer4InVisionAngle()
+    {
+        Vector3 playerDirection = player4.transform.position - transform.position;
         playerDirection = playerDirection.normalized;
 
         float dot = Vector3.Dot(playerDirection, transform.forward);
