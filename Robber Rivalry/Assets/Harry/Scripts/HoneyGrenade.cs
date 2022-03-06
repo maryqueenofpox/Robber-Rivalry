f2ccb0e3677cf24f8a22bd7ccbfc18f12d79b5c2
Harry Melhell <hcalwe200@caledonian.ac.uk>
2022-03-06 22:42:29 +0000

    Honey Grenade

---
+++ Robber Rivalry/Assets/Harry/Scripts/HoneyGrenade.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoneyGrenade : MonoBehaviour
+{
+
+    float activeDuration = 3f;
+    [SerializeField] Transform HoneySplat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activeDuration -= Time.deltaTime;
+        if (activeDuration <= 0.2f)
+        {
+            Transform clone;
+            clone = Instantiate(HoneySplat, transform.position, HoneySplat.rotation);
+            clone.gameObject.tag = "HoneySplat";
+        }
+        if (activeDuration <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
