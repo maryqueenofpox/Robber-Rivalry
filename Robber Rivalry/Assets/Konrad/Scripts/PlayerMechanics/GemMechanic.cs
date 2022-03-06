080829a6f8089e83afd35292d8a751cb004ffaa1
IDrinkMyTea <92086626+IDrinkMyTea@users.noreply.github.com>
2022-03-06 00:09:16 +0000

    Quick push so I can pull
    Nothing important.

--- Robber Rivalry/Assets/Konrad/Scripts/PlayerMechanics/GemMechanic.cs
+++ Robber Rivalry/Assets/Konrad/Scripts/PlayerMechanics/GemMechanic.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class GemMechanic : MonoBehaviour
 {
+    public bool isCarryingGem = false;
+    [Header("Gem Score")]
+    [SerializeField] float gemPointIncrease;
+    public float timeUntilScoreIncrease = 4.0f;
+    float originalTimeUntilScoreIncrease;
+    Transform gemTransform;
+    bool isGem;
+    Transform gemChild;
+    LootGrabber lootGrabber;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lootGrabber = GetComponent<LootGrabber>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isCarryingGem)
+        {
+            if (transform.childCount < 2)
+                isCarryingGem = false;
+            else
+            {
+                timeUntilScoreIncrease -= Time.deltaTime;
+                if (timeUntilScoreIncrease <= 0.0f)
+                {
+                    lootGrabber.loot += gemPointIncrease;
+                    lootGrabber.score.text = lootGrabber.loot.ToString();
+                    timeUntilScoreIncrease = originalTimeUntilScoreIncrease;
+                }
+            }
+        }
     }
 }
--- Robber Rivalry/Assets/Konrad/Scripts/PlayerMechanics/SlapMechanic.cs
+++ Robber Rivalry/Assets/Konrad/Scripts/PlayerMechanics/SlapMechanic.cs
@@ -15,9 +15,15 @@ public class SlapMechanic : MonoBehaviour
     bool doTheSlap = false;
     float originalSlapCooldown;
 
+    Animator anim;
+
+    public PlayerControls controls { get; set; }
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+
         originalSlapToFalse = timeToSetSlapToFalse;
 
         originalSlapToFalse = timeToSetSlapToFalse;
@@ -27,6 +33,82 @@ public class SlapMechanic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (anim.GetBool("isSlapping") == true)
+        {
+            timeToSetSlapToFalse -= Time.deltaTime;
+            if (timeToSetSlapToFalse <= 0f)
+            {
+                timeToSetSlapToFalse = originalSlapToFalse;
+                anim.SetBool("isSlapping", false);
+            }
+        }
+        /*
+        if (isStunned && vulnerable)
+        {
+            stunDuration -= Time.deltaTime;
+            anim.SetBool("gotSlapped", true);
+            if (stunDuration <= 0)
+            {
+                anim.SetBool("gotSlapped", false);
+                isStunned = false;
+                vulnerable = false;
+                stunDuration = originalStunDuration;
+                anim.SetBool("idle", true);
+            }
+        }
+        */
+        if (!canSlap)
+        {
+            slapCooldown -= Time.deltaTime;
+        }
+
+        if (slapCooldown <= 0.0f)
+        {
+            canSlap = true;
+            slapCooldown = originalSlapCooldown;
+        }
+
+        if (doTheSlap)
+            DoTheSlap();
+    }
+
+    void DoTheSlap()
+    {
+        /*
+        if (canSlap && !isStunned)
+        {
+            anim.SetBool("isSlapping", true);
+            if (timeToSetSlapToFalse <= 0.1f)
+            {
+                if (isPlayer && controls.vulnerable)
+                {
+                    if (controls.forceField.enabled == true)
+                    {
+                        controls.forceField.enabled = false;
+                        canSlap = false;
+                        doTheSlap = false;
+                        return;
+                    }
+                    else
+                    {
+                        controls.rb.AddForce(transform.forward * slapFoce);
+                        //if (controls.vulnerable)
+                        controls.isStunned = true;
+
+                        if (controls.isCarryingGem)
+                        {
+                            controls.transform.Find("Gem").transform.localPosition = controls.transform.TransformDirection(-Vector3.forward * 2);
+                            controls.transform.Find("Gem").transform.parent = null;
+                            controls.isCarryingGem = false;
+                        }
+
+                        canSlap = false;
+                        doTheSlap = false;
+                    }
+                }
+                doTheSlap = false;
+            }
+        }
+        */
     }
 }
