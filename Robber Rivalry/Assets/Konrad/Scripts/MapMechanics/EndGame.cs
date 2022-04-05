e76d7cd8410c1292ebd8834f19c34034351e2a31
SleepyGanyu <77534705+SleepyGanyu@users.noreply.github.com>
2022-04-06 00:47:10 +0100

    End game screen added
    Just need to deal with the hell that is getting it to identify multiple winners and displaying that properly ;-;. I have a vague solution but its b-tech as helllllll.
    
    -Removed Amongus

--- Robber Rivalry/Assets/Konrad/Scripts/MapMechanics/EndGame.cs
+++ Robber Rivalry/Assets/Konrad/Scripts/MapMechanics/EndGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 using UnityEngine.EventSystems;
 
 public class EndGame : MonoBehaviour
@@ -13,7 +14,7 @@ public class EndGame : MonoBehaviour
     [SerializeField] float gemRewardAmount;
     public Image fuse;
     public float fuseTimer = 20f;
-    [SerializeField] GameObject firstButtonToBeSelected;
+    
 
     [SerializeField] AudioSource fuseSound;
     [SerializeField] float penalty = 5f;
@@ -42,29 +43,61 @@ public class EndGame : MonoBehaviour
     [SerializeField] LootGrabber player_3_Script;
     [SerializeField] LootGrabber player_4_Script;
 
-    [Header("Player Loss Icon")]
-    [SerializeField] GameObject Player_1_Loss;
-    [SerializeField] GameObject Player_2_Loss;
-    [SerializeField] GameObject Player_3_Loss;
-    [SerializeField] GameObject Player_4_Loss;
+    [Header("Player 2nd Icon")]
+    [SerializeField] GameObject Player_1_Second;
+    [SerializeField] GameObject Player_2_Second;
+    [SerializeField] GameObject Player_3_Second;
+    [SerializeField] GameObject Player_4_Second;
+
+    [Header("Player 3rd Icon")]
+    [SerializeField] GameObject Player_1_Third;
+    [SerializeField] GameObject Player_2_Third;
+    [SerializeField] GameObject Player_3_Third;
+    [SerializeField] GameObject Player_4_Third;
+
+    [Header("Player 4th Icon")]
+    [SerializeField] GameObject Player_1_Fourth;
+    [SerializeField] GameObject Player_2_Fourth;
+    [SerializeField] GameObject Player_3_Fourth;
+    [SerializeField] GameObject Player_4_Fourth;
 
     float max;
     bool doOnce;
     bool doGemAddOnce;
     bool playOnce;
 
+    [Header("Arrays")]
+    float[] ranking = new float[4];
+    GameObject[] Player1Rankings = new GameObject[4];
+    GameObject[] Player2Rankings = new GameObject[4];
+    GameObject[] Player3Rankings = new GameObject[4];
+    GameObject[] Player4Rankings = new GameObject[4];
+    TextMeshProUGUI[] ScoreRanking = new TextMeshProUGUI[4];
+
     // Start is called before the first frame update
     void Start()
     {
         endGamePanel.SetActive(false);
+
         player_1_Crown.SetActive(false);
         player_2_Crown.SetActive(false);
         player_3_Crown.SetActive(false);
         player_4_Crown.SetActive(false);
-        Player_1_Loss.SetActive(false);
-        Player_2_Loss.SetActive(false);
-        Player_3_Loss.SetActive(false);
-        Player_4_Loss.SetActive(false);
+
+        Player_1_Second.SetActive(false);
+        Player_2_Second.SetActive(false);
+        Player_3_Second.SetActive(false);
+        Player_4_Second.SetActive(false);
+
+        Player_1_Third.SetActive(false);
+        Player_2_Third.SetActive(false);
+        Player_3_Third.SetActive(false);
+        Player_4_Third.SetActive(false);
+
+        Player_1_Fourth.SetActive(false);
+        Player_2_Fourth.SetActive(false);
+        Player_3_Fourth.SetActive(false);
+        Player_4_Fourth.SetActive(false);
 
         playOnce = false;
         doOnce = true;
@@ -86,6 +119,29 @@ public class EndGame : MonoBehaviour
 
             AddGemPoints();
             max = Mathf.Max(player_1_Script.loot, player_2_Script.loot, player_3_Script.loot, player_4_Script.loot);
+
+            //Assigns player scores to an array, and sorts them lowest (0) to highest (3)
+            ranking[0] = player_1_Script.loot;
+            ranking[1] = player_2_Script.loot;
+            ranking[2] = player_3_Script.loot;
+            ranking[3] = player_4_Script.loot;
+            Array.Sort(ranking);
+
+            EnableCrown();
+
+            //Assigns player score textmeshes to ScoreRanking array
+            ScoreRanking[0] = player_4_Score;
+            ScoreRanking[1] = player_3_Score;
+            ScoreRanking[2] = player_2_Score;
+            ScoreRanking[3] = player_1_Score;
+
+            //For loop that lasts until i is one less than array.length
+            for (int i = 0; i < ScoreRanking.Length; i++)
+            {
+                //assigns the scores into their respective textmeshes (1st place score in 1st place text mesh)
+                ScoreRanking[i].text = ranking[i].ToString();
+            }
+
             Scoring();
             if (player_1_Script.loot < 0)
             {
@@ -202,43 +258,74 @@ public class EndGame : MonoBehaviour
             doOnce = false;
         }
 
-        player_1_Score.text = player_1_Script.loot.ToString();
-        player_2_Score.text = player_2_Script.loot.ToString();
-        player_3_Score.text = player_3_Script.loot.ToString();
-        player_4_Score.text = player_4_Script.loot.ToString();
+        // Should be safe to remove these since theyre now captured in the new loop. Could move that here aswell and should work. Enabling this breaks the loop stuff so pls dont ;-;
+
+        // player_1_Score.text = player_1_Script.loot.ToString();
+        // player_2_Score.text = player_2_Script.loot.ToString();
+        // player_3_Score.text = player_3_Script.loot.ToString();
+        // player_4_Score.text = player_4_Script.loot.ToString();
 
     }
 
     void EnableCrown()
     {
-        if (player_1_Script.loot == max)
+        // Assings player sprites to their respective Arrays
+        Player1Rankings[0] = Player_1_Fourth;
+        Player1Rankings[1] = Player_1_Third;
+        Player1Rankings[2] = Player_1_Second;
+        Player1Rankings[3] = player_1_Crown;
+
+        Player2Rankings[0] = Player_2_Fourth;
+        Player2Rankings[1] = Player_2_Third;
+        Player2Rankings[2] = Player_2_Second;
+        Player2Rankings[3] = player_2_Crown;
+
+        Player3Rankings[0] = Player_3_Fourth;
+        Player3Rankings[1] = Player_3_Third;
+        Player3Rankings[2] = Player_3_Second;
+        Player3Rankings[3] = player_3_Crown;
+
+        Player4Rankings[0] = Player_4_Fourth;
+        Player4Rankings[1] = Player_4_Third;
+        Player4Rankings[2] = Player_4_Second;
+        Player4Rankings[3] = player_4_Crown;
+
+        // For loop that will run until i is one less than array length
+        for (int i = 0; i < ranking.Length; i++)
         {
-            player_1_Crown.SetActive(true);
-            Player_2_Loss.SetActive(true);
-            Player_3_Loss.SetActive(true);
-            Player_4_Loss.SetActive(true);
+            // Checks for player 1's loot and compares to each ranked score value
+            if (player_1_Script.loot == ranking[i] )
+            {
+                // Will enable the sprite that corresponds with the players point value (second place loss for second place points)
+                Player1Rankings[i].SetActive(true);
+            }
         }
-        if (player_2_Script.loot == max)
+
+        for (int i = 0; i < ranking.Length; i++)
         {
-            player_2_Crown.SetActive(true);
-            Player_1_Loss.SetActive(true);
-            Player_3_Loss.SetActive(true);
-            Player_4_Loss.SetActive(true);
+            if (player_2_Script.loot == ranking[i])
+            {
+                Player2Rankings[i].SetActive(true);
+            }
         }
-        if (player_3_Script.loot == max)
+
+        for (int i = 0; i < ranking.Length; i++)
         {
-            player_3_Crown.SetActive(true);
-            Player_1_Loss.SetActive(true);
-            Player_2_Loss.SetActive(true);
-            Player_4_Loss.SetActive(true);
+            if (player_3_Script.loot == ranking[i])
+            {
+                Player3Rankings[i].SetActive(true);
+            }
         }
-        if (player_4_Script.loot == max)
+
+        for (int i = 0; i < ranking.Length; i++)
         {
-            player_4_Crown.SetActive(true);
-            Player_1_Loss.SetActive(true);
-            Player_2_Loss.SetActive(true);
-            Player_3_Loss.SetActive(true);
+            if (player_4_Script.loot == ranking[i])
+            {
+                Player4Rankings[i].SetActive(true);
+            }
         }
+
+
     }
 
     private void OnTriggerEnter(Collider other)
